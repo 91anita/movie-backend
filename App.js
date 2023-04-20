eec9b45405d2ad8982d7ee3479dc90ee2d3afc92
91anita <45237162+91anita@users.noreply.github.com>
2023-04-20 14:12:32 +0530

    Making apps error proof(handling loading)

--- App.js
+++ App.js
@@ -5,8 +5,10 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
+const [isLoading,setIsLoading]= useState(false);
 
   async function fetchMovieHandler() {
+    setIsLoading(true);
    const response = await fetch("https://swapi.dev/api/films")
       const data = await response.json();
 
@@ -19,7 +21,8 @@ function App() {
           };
         });
 
-        setMovies(transformMovies);;
+        setMovies(transformMovies);
+        setIsLoading(false);
   }
 
   return (
@@ -28,7 +31,9 @@ function App() {
         <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found no Movies</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
